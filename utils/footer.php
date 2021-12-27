7cd9183f7b463e2b90008569452047dc90eeaf71
Kruthi-R-Aithal <96725814+Kruthi-R-Aithal@users.noreply.github.com>
2021-12-27 18:49:23 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,28 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>D</strong><span class = "small footerSubtext">ayananda</span>
+                        <strong>S</strong><span class = "small footerSubtext">agar</span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege of</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngineering</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Kumarswamy Layout
+                        &copy; Ven-You, 2021
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Devops Project<br>
+                    Information Science and Engineering<br>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
